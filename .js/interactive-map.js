037d517c640fd0f836a3aee20d05af2ac6ffbb89
Yraya Alonso Monzón <yrayaalonsomonzon@alumno.ieselrincon.es>
2026-03-27 14:02:54 +0000

    Version_27/03/2026_3

---
+++ .js/interactive-map.js
@@ -0,0 +1,23 @@
+
+let mapa = document.getElementById("mapa");
+
+document.getElementById("grecia").addEventListener("click", function() {
+mapa.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12935704.63503737!2d13.737131391165939!3d37.66962916719039!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x135b4ac711716c63%3A0x363a1775dc9a2d1d!2sGrecia!5e0!3m2!1ses!2ses!4v1773687892542!5m2!1ses!2ses";
+});
+
+document.getElementById("escandinavia").addEventListener("click", function() {
+mapa.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15692495.01086658!2d-2.872344586441085!3d61.30682254756606!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4664bcea4d8e22cd%3A0x7186faed0155b381!2sPen%C3%ADnsula%20escandinava!5e0!3m2!1ses!2ses!4v1773688176692!5m2!1ses!2ses";
+});
+
+document.getElementById("islandia").addEventListener("click", function() {
+mapa.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15692495.01086658!2d-2.872344586441085!3d61.30682254756606!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48d22b52a3eb6043%3A0x6f8a0434e5c1459a!2sIslandia!5e0!3m2!1ses!2ses!4v1773688254339!5m2!1ses!2ses";
+});
+
+document.getElementById("noruega").addEventListener("click", function() {
+mapa.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3470812.535228951!2d-19.24804999999997!3d64.86425!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x461268458f4de5bf%3A0xa1b03b9db864d02b!2sNoruega!5e0!3m2!1ses!2ses!4v1773688301781!5m2!1ses!2ses";
+});
+
+document.getElementById("egipto").addEventListener("click", function() {
+mapa.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14624237.032542692!2d20.250714098916053!3d26.508210354011997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14368976c35c36e9%3A0x2c45a00925c4c444!2sEgipto!5e0!3m2!1ses!2ses!4v1773688345272!5m2!1ses!2ses";
+});
+
